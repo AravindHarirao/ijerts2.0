af6c662d3898995f58ccbeb7f709a92b2a7a9ad2
senthil <senthilavs@gmail.com>
2019-04-25 23:39:22 +0530

    scripts added current issues files added

---
+++ DBScripts/currentissues.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `currentissues` (
+  `issueId` int(11) NOT NULL AUTO_INCREMENT,
+  `paperName` varchar(500) DEFAULT NULL,
+  `paperPath` varchar(500) DEFAULT NULL,
+  `sortorder` int(11) DEFAULT NULL,
+  `numberofDownload` int(11) DEFAULT NULL,
+  `createdDate` datetime DEFAULT NULL,
+  `createdBy` varchar(255) DEFAULT NULL,
+  `updatedDate` datetime DEFAULT NULL,
+  `updatedBy` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`issueId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `ijerts_live`.`currentissues` (`issueId`, `paperName`, `paperPath`, `sortorder`, `numberofDownload`, `createdDate`, `createdBy`, `updatedDate`, `updatedBy`) VALUES ('1', 'GREENHOUSE MONITORING AND CONTROLLING SYSTEM BY USING ANDROID', 'Paper ID-IJERTS_2019_001.pdf', '1', '0', '2019-04-25 22:00:00', 'System', '2019-04-25 22:00:00', 'System');
+INSERT INTO `ijerts_live`.`currentissues` (`issueId`, `paperName`, `paperPath`, `sortorder`, `numberofDownload`, `createdDate`, `createdBy`, `updatedDate`, `updatedBy`) VALUES ('2', 'AN EFFICIENT APPROACH FOR SEGMENTATION AND CLASSIFICATION OF BRAIN TUMOR MRI IMAGES', 'Paper ID-IJERTS_2019_002.pdf', '2', '0', '2019-04-25 22:00:00', 'System', '2019-04-25 22:00:00', 'System');
+INSERT INTO `ijerts_live`.`currentissues` (`issueId`, `paperName`, `paperPath`, `sortorder`, `numberofDownload`, `createdDate`, `createdBy`, `updatedDate`, `updatedBy`) VALUES ('3', 'AN INTELLIGENT ROBOTICS SYSTEM FOR PICKING UP OF FLOWERS AND VEGETABLES', 'Paper IDIJERTS_2019_003.pdf', '3', '0', '2019-04-25 22:00:00', 'System', '2019-04-25 22:00:00', 'System');
+INSERT INTO `ijerts_live`.`currentissues` (`issueId`, `paperName`, `paperPath`, `sortorder`, `numberofDownload`, `createdDate`, `createdBy`, `updatedDate`, `updatedBy`) VALUES ('4', 'SMART CONTROLLING USING IOT', 'Paper IDIJERTS_2019_004.pdf', '4', '0', '2019-04-25 22:00:00', 'System', '2019-04-25 22:00:00', 'System');
+INSERT INTO `ijerts_live`.`currentissues` (`issueId`, `paperName`, `paperPath`, `sortorder`, `numberofDownload`, `createdDate`, `createdBy`, `updatedDate`, `updatedBy`) VALUES ('5', 'AN EFFICIENT INVESTIGATION OF HUMAN STRESS BY USING SUPPORT VECTOR MACHINE (SVM)', 'Paper IDIJERTS_2019_005.pdf', '5', '0', '2019-04-25 22:00:00', 'System', '2019-04-25 22:00:00', 'System');
+INSERT INTO `ijerts_live`.`currentissues` (`issueId`, `paperName`, `paperPath`, `sortorder`, `numberofDownload`, `createdDate`, `createdBy`, `updatedDate`, `updatedBy`) VALUES ('6', 'GREENHOUSE MONITORING AND CONTROLLING SYSTEM BY USING ANDROID', 'Paper ID-IJERTS_2019_007.pdf', '6', '0', '2019-04-25 22:00:00', 'System', '2019-04-25 22:00:00', 'System');
+INSERT INTO `ijerts_live`.`currentissues` (`issueId`, `paperName`, `paperPath`, `sortorder`, `numberofDownload`, `createdDate`, `createdBy`, `updatedDate`, `updatedBy`) VALUES ('7', 'DESIGN OF LOW COST VACUUM ASSISTED CLOSURE WOUND THERAPY', 'Paper ID-IJERTS_2019_008.pdf', '7', '0', '2019-04-25 22:00:00', 'System', '2019-04-25 22:00:00', 'System');
+INSERT INTO `ijerts_live`.`currentissues` (`issueId`, `paperName`, `paperPath`, `sortorder`, `numberofDownload`, `createdDate`, `createdBy`, `updatedDate`, `updatedBy`) VALUES ('8', 'DESIGN OF OPTICAL LINK FOR OPTICAL NANO ANTENNAS', 'PAPER ID-IJERTS_2019_010.pdf', '8', '0', '2019-04-25 22:00:00', 'System', '2019-04-25 22:00:00', 'System');
+INSERT INTO `ijerts_live`.`currentissues` (`issueId`, `paperName`, `paperPath`, `sortorder`, `numberofDownload`, `createdDate`, `createdBy`, `updatedDate`, `updatedBy`) VALUES ('9', 'DESIGN OF LOW POWER PULSE GENERATES FLIPFLOP DESIGN BASED ON A SIGNAL FEED-THROUGH SCHEME ', 'PAPER ID-IJERTS_2019_011.pdf', '9', '0', '2019-04-25 22:00:00', 'System', '2019-04-25 22:00:00', 'System');
+INSERT INTO `ijerts_live`.`currentissues` (`issueId`, `paperName`, `paperPath`, `sortorder`, `numberofDownload`, `createdDate`, `createdBy`, `updatedDate`, `updatedBy`) VALUES ('10', 'COAL MINING ACCIDENT RESCUE SYSTEMS', 'PAPER ID-IJERTS_2019_012.pdf', '10', '0', '2019-04-25 22:00:00', 'System', '2019-04-25 22:00:00', 'System');
