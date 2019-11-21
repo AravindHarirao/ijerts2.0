27aff0e2957265c2c1f61f21eaca37c024844fca
senthil <senthilavs@gmail.com>
2019-11-21 23:48:14 +0530

    added the editor dashboard object

---
+++ IJERTS.Objects/EditorDashboard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IJERTS.Objects
+{
+    public class EditorDashboard : IJERTSAbstract
+    {
+        public List<Paper> LstPapers { get; set; }
+
+        public List<Users> LstUsers { get; set; }
+
+        public List<Queries> LstQueries { get; set; }
+
+    }
+}
